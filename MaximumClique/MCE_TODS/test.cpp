c728e010751bc0ecaaff85271e7838d2628c3090
rajagopal067 <raj.bhojan@gmail.com>
2016-04-12 16:17:43 -0700

    added code for cliques

---
+++ MaximumClique/MCE_TODS/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+int main(int argc, char *argv[])
+{
+    std::cout << "There are " << argc << " arguments:\n";
+ 
+    // Loop through each argument and print its number and value
+    for (int count=0; count < argc; ++count)
+        std::cout << count << " " << argv[count] << '\n';
+ 
+    return 0;
+}
